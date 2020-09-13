f02d6afdb56fbf6e0a7260782be88db1062e1f19
Codê <scm@MacBook-Pro-de-SCM.local>
2020-09-13 18:09:06 +0100

    Hello SpringBoot and Rest

---
+++ 01 RestWithSpringBootUdemy Scaffold Eclipse/src/main/java/ao/co/code2f/Startup.java
@@ -0,0 +1,13 @@
+package ao.co.code2f;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Startup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Startup.class, args);
+	}
+
+}
